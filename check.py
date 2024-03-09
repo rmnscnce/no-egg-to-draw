9c1b82235fe0fe13650b44cb4063b44cbd35ecd1
Paskal Sitepu <rmnscnce@ya.ru>
2024-03-09 09:15:10 +0700

    treewide: Pre-strip files of any whitespace

--- check.py
+++ check.py
@@ -4,8 +4,8 @@ for path, _, file in os.walk("identities"):
     for f in file:
         if f != ".keep":
             with open(os.path.join(path, f), "r") as singleton:
-                content = singleton.read()
-                if not content.strip():
+                content = singleton.read().strip()
+                if not content:
                     print(f"File {f} is empty or contains only whitespace")
                     exit(1)
                 if len(content.split("\n")) > 1:
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ for path, _, file in os.walk("identities"):
     for f in file:
         if f != ".keep":
             with open(os.path.join(path, f), "r") as singleton:
-                entries.append(singleton.read())
+                entries.append(singleton.read().strip())
 
 with open("identities_finalized.txt", "w") as muxed:
     muxed.write("\n".join(entries))
